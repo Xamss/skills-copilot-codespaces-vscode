2f6bc8bc7193ff507cabf6648d405ad4db254c5b
Xamss <78305816+Xamss@users.noreply.github.com>
2023-08-10 09:44:57 +0000

    Copilot second ommit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var member2 = document.getElementById("member2");
+}
